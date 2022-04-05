8a6cc90d5c0fb4018fed392e59698a3d09903522
Abideen Kareem <abideenkareem17@gmail.com>
2022-04-05 10:19:42 -0500

    Update createInstance

--- createInstance.tf
+++ createInstance.tf
@@ -1,5 +1,10 @@
+provider "aws" {
+  access_key = "AKIAWEVMRXDGLRGEWO5E"
+  secret_key = "SECRET_KEY"
+  region = "us-east-1"
+}
 
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = "ami-05692172625678b4e"
+  ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t2.micro"
 }
